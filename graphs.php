c12291c8b7bff35a5e43b1c86b15a708a198c73d
Miguel Almeida <miguelalmeida2002@gmail.com>
2022-08-22 00:44:05 +0100

    Production Control

--- graphs.php
+++ graphs.php
@@ -56,7 +56,7 @@
 
                         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
                            data-bs-toggle="dropdown">
-                            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                            <img src="assets/img/profile.png" alt="Profile" class="rounded-circle">
                             <span class="d-none d-md-block dropdown-toggle ps-2">
                                 <?php
                                     echo $_SESSION["username"]
